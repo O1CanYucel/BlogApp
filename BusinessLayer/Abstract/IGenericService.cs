b516995c95c0f8d6a3a5d6c443233ba3e43d4cd1
Ömer Can Yücel <omer.canyucel@outlook.com>
2022-03-29 15:20:39 +0300

    Add project files.

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        public void Add<T> (T item);
+        public void Remove<T> (T item);
+        public void Update<T> (T item);
+        public List<T> GetAll<T> ();
+        public T Get<T> (int id);  
+    }
+}
---
+++ BusinessLayer/Concrete/GenericManager.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    internal class GenericManager<T> : IGenericService<T> where T : class
+    {
+        GenericRepository<T> repository = new GenericRepository<T>();
+        public void Add<T1>(T1 item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T1 Get<T1>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T1> GetAll<T1>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove<T1>(T1 item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T1>(T1 item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
